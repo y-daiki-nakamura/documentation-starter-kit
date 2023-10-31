14ff68411df020fdc57499f81cdb2b42d55548cb
Daiki Nakamura <55738391+y-daiki-nakamura@users.noreply.github.com>
2023-10-31 07:46:11 +0000

    fix

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,21 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Nextra Sample Project</span>,
-
+  logo: <span>My Project</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
-  themeSwitch: {
-    useOptions() {
-      return {
-        light: 'Light',
-        dark: 'Dark',
-        system: 'System'
-      }
-    }
+  chat: {
+    link: 'https://discord.com',
   },
-  
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
 }
 
 export default config
