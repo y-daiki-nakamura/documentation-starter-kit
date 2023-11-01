6d578b4a1cb09637568eb15d47628af0766507db
Daiki Nakamura <55738391+y-daiki-nakamura@users.noreply.github.com>
2023-11-01 16:34:58 +0000

    add protect route

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+//https://auth0.github.io/nextjs-auth0/types/helpers_with_middleware_auth_required.WithMiddlewareAuthRequired.html
+
+//すべてのルートを保護する
+
+import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
+
+export default withMiddlewareAuthRequired();
+
+export const config = {
+    matcher: '/about/:path*',
+  };
