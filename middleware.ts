749453e0398c16ac4e06fe954b560cf37c751683
Daiki Nakamura <55738391+y-daiki-nakamura@users.noreply.github.com>
2024-08-13 18:08:52 +0900

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,5 @@ import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge';
 export default withMiddlewareAuthRequired();
 
 export const config = {
-    matcher: '/about/:path*',
+    matcher: '/:path*',
   };
